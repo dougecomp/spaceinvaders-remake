39cfbd6c31012d9f86ead599322f5b62952976c4
brunosousa <brunosousa@9e9c5a6a-d451-46b5-9342-b024e54f8c71>
2014-05-28 19:28:51 +0000

    Refatoracao do metodo verificachoquealien.

--- spaceinvaders/src/mediator/ImpactMediator.java
+++ spaceinvaders/src/mediator/ImpactMediator.java
@@ -2,6 +2,8 @@ package mediator;
 
 import java.awt.Rectangle;
 
+import javax.swing.ImageIcon;
+
 import model.Alien;
 import model.Nave;
 import model.Tiro;
@@ -25,12 +27,36 @@ public class ImpactMediator implements Mediator{
 	@Override
 	public void verificarChoque(Colleague him) {
 		// TODO Auto-generated method stub
+		if(me instanceof NaveColleague){ // colide com alien ou nave			
+			verificaChoqueNave(him);
+			
+		} else if(me instanceof TiroColleague){ // colide com tiro de nave e com a nave
+			
+			TiroColleague colTiro = (TiroColleague) me;
+			Tiro tiro = colTiro.getTiro();
+			Rectangle recTiro = tiro.getBounds();
+			
+			if(him instanceof AlienColleague) {
+				AlienColleague colAlien = (AlienColleague) him;
+				Alien alien = colAlien.getAlien();
+				
+				ImageIcon icon = new ImageIcon("res//Explosao.gif");
+				Rectangle recAlien = alien.getBounds();
+				if(recTiro.intersects(recAlien)){
+					alien.setImagem(icon.getImage());
+					alien.setVisible(false);
+					tiro.setVisible(false);
+					tiro.disparaMatouAlien();
+				}
+			}
+
 		if(me instanceof NaveColleague){ // colisão com alien ou tiro de alien
 			verificaChoqueNave(him);
 		} else if(me instanceof TiroColleague){ // colisão com alien ou nave
 			verificaChoqueTiro(him);
+
 		}
-	}
+	}}
 	
 	/**
 	 * Verifica Colisão de Alien com Nave e Tiro de Nave
@@ -43,15 +69,20 @@ public class ImpactMediator implements Mediator{
 		NaveColleague colNave = (NaveColleague) me;
 		Nave nave = colNave.getNave();
 		Rectangle recNave = nave.getBounds();
+
 		if(him instanceof AlienColleague){ // Colisão de nave com alien
+
 			AlienColleague colAlien = (AlienColleague) him;
 			Alien alien = colAlien.getAlien();
 			Rectangle recAlien = alien.getBounds();				
 			if(recNave.intersects(recAlien)){
+
 				int vidas = nave.getVidas()- 1;
 				nave.setVidas(vidas);
+
 				nave.setVisible(false);
 				alien.setVisible(false);
+				
 			}
 		} else if(him instanceof TiroColleague){ // colisão de alien com tiro (verificar se é da nave)
 			TiroColleague colTiro = (TiroColleague) him;
@@ -96,6 +127,6 @@ public class ImpactMediator implements Mediator{
 				tiro.setVisible(false);
 				tiro.disparaMatouPlayer();
 			}
-		}
-	}
+		}	
 }
+	}
